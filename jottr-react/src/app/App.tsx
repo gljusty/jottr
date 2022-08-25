4affa5396f75dcafc610e94f0a18a55e5452bbed
Bryan Stapleton <stapleton_bryan@yahoo.com>
2022-08-25 07:29:06 -0600

    re org

---
+++ jottr-react/src/app/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <button
+        onClick={() => {
+          setCount((count) => count + 1);
+        }}
+      >
+        b
+      </button>
+      <div>{count}</div>
+    </div>
+  );
+}
+
+export default App;
