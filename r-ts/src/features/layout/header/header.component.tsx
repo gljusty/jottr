c98ce3ffa504d664c6b2f1ecbbf5ea3966851e3d
Bryan Stapleton <stapleton_bryan@yahoo.com>
2022-10-15 22:00:57 -0600

    minor header changes

--- r-ts/src/features/layout/header/header.component.tsx
+++ r-ts/src/features/layout/header/header.component.tsx
@@ -11,25 +11,15 @@ import { TbCheck, TbBallon, TbBoxMultiple } from "react-icons/tb";
 const JottrHeader = <T extends Partial<HeaderProps>>({}: T) => {
   const theme = useMantineTheme();
   return (
-    <Header
-      sx={{
-        padding: theme.spacing.md,
-        background:
-          theme.colorScheme == "dark"
-            ? `linear-gradient(${theme.black[10]} 0%, whitesmoke 100%)`
-            : "gray",
-      }}
-      height={60}
-    >
+    <Header p={"md"} height={60}>
       <Group position={"apart"} spacing={"sm"}>
         <Group position={"center"} spacing={"xs"}>
           #Brand
           <TbBallon size={24} />
         </Group>
         <Group position={"center"} spacing={"sm"}>
-          <ThemeIcon>
-            <TbCheck size={24} />
-          </ThemeIcon>
+          <TbCheck size={24} />
+
           <ActionIcon>
             <TbBoxMultiple />
           </ActionIcon>
